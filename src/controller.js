d0d2bcc2c34ed77d7bbbcca10940cd9e8d16ec6d
dean131 <deanabdillah@gmail.com>
2024-09-10 09:08:33 +0700

    Refactor controller.js to include explicit route for subdomainly endpoint and update docker-compose command

--- src/controller.js
+++ src/controller.js
@@ -6,7 +6,7 @@ const subdomainly = (req, res) => {
 
     // Menjalankan perintah git pull dan docker-compose
     exec(
-        "cd /home/dean/Projects/subdomainly && git pull && docker compose down && docker compose build && docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d",
+        "cd /home/dean/Projects/subdomainly && git pull && docker compose down && docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build",
         (error, stdout, stderr) => {
             if (error) {
                 console.error(`exec error: ${error}`);
