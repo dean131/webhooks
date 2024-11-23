28803cd86a39df8d51192d8178acbc07b527bb49
dean131 <deanabdillah@gmail.com>
2024-11-23 04:22:47 +0000

    Refactor docker-compose and controller.js for improved command execution and network configuration

--- src/controller.js
+++ src/controller.js
@@ -4,10 +4,14 @@ const subdomainly = (req, res) => {
     // Verifikasi payload jika perlu (misalnya, verifikasi signature GitHub)
     console.log("PUSH EVENT RECEIVED: SUBDOMAINLY");
 
+    const path = "/home/dean-cloud/github/subdomainly";
+    const git = "git pull";
+    const dockerCompose = "docker compose down && docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build";
+    const command = `cd ${path} && ${git} && ${dockerCompose}`;
+
     // Menjalankan perintah git pull dan docker-compose
-    exec(
-        "cd /home/dean/Projects/subdomainly && git pull && docker compose down && docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build",
-        (error, stdout, stderr) => {
+    try {
+        exec(command, (error, stdout, stderr) => {
             if (error) {
                 console.error(`exec error: ${error}`);
                 return res.status(500).send("Pull and deploy failed");
@@ -17,8 +21,11 @@ const subdomainly = (req, res) => {
             console.error(`stderr: ${stderr}`);
 
             res.status(200).send("Pull and deploy successful");
-        }
-    );
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal server error");
+    }
 };
 
 const posyandu  = (req, res) => {
